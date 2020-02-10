0e74e944bdc851a8fd9c8c0d6b6a59e9d0113088
sirjasongo <jasongo@sparkresearch.net>
2020-02-10 15:30:07 +0800

    improved

--- ManyToMany.php
+++ ManyToMany.php
@@ -5,10 +5,10 @@ trait ManyToMany
 {
     private $_m2m = [];
 
-    public function hasManyToMany($target_class, $bridge_class, $default_field)
+    public function hasManyToMany($target_class, $bridge_class, $default_field='id')
     {
-        $target = $this->get_class_name($target_class);
-        $bridge = $this->get_class_name($bridge_class);
+        $target = basename($target_class);
+        $bridge = basename($bridge_class);
         $this_their_field = $this->get_their_field($this, $bridge_class); //!empty($this::their_field[$bridge_class]) ? $this::their_field[$bridge_class] : (is_string($this::their_field) ? $this::their_field : $this::their_field[0]);
         
         $this->hasMany($bridge, [$bridge_class, 'our_field'=> $this::our_field, 'their_field'=>$this_their_field]);
@@ -21,11 +21,11 @@ trait ManyToMany
 
             if (!empty($data)) {
                 $data = is_array($data) ? $data : explode(',', $data);
-                return $this->ref($this->get_class_name($bridge_class))->ref($this->get_class_name($target_class))->addCondition($field, $data);
+                return $this->ref(basename($bridge_class))->ref(basename($target_class))->addCondition($field, $data);
             }
             else
             {
-                return $this->ref($this->get_class_name($bridge_class))->ref($this->get_class_name($target_class));
+                return $this->ref(basename($bridge_class))->ref(basename($target_class));
             }
         });
 
@@ -35,7 +35,7 @@ trait ManyToMany
             
             $field = empty($field) ? $default_field : $field;
             if (!$this->loaded()) {
-                throw new \Exception('No data loaded for ' . $this->get_class_name($this) .'. ' . $this->get_class_name($target_class) . ' can not be accessed without a dataset loaded.');
+                throw new \Exception('No data loaded for ' . basename($this) .'. ' . basename($target_class) . ' can not be accessed without a dataset loaded.');
             }
 
             $this_their_field = $this->get_their_field($this, $bridge_class); //!empty($this::their_field[$bridge_class]) ? $this::their_field[$bridge_class] : (is_string($this::their_field) ? $this::their_field : $this::their_field[0]);
@@ -60,7 +60,7 @@ trait ManyToMany
                     return false;
                 }
             }
-            return $this->ref($this->get_class_name($bridge_class))->ref($this->get_class_name($target_class));
+            return $this->ref(basename($bridge_class))->ref(basename($target_class));
         });
 
         $this->_m2m['remove'.$target] = [$target_class, $bridge_class, $default_field];
@@ -69,7 +69,7 @@ trait ManyToMany
             
             $field = empty($field) ? $default_field : $field;
             if (!$this->loaded()) {
-                throw new \Exception('No data loaded for ' . $this->get_class_name($this) .'. ' . $this->get_class_name($target_class) . ' can not be accessed without a dataset loaded.');
+                throw new \Exception('No data loaded for ' . basename($this) .'. ' . basename($target_class) . ' can not be accessed without a dataset loaded.');
             }
 
             $this_their_field = $this->get_their_field($this, $bridge_class); //!empty($this::their_field[$bridge_class]) ? $this::their_field[$bridge_class] : (is_string($this::their_field) ? $this::their_field : $this::their_field[0]);
@@ -81,7 +81,7 @@ trait ManyToMany
                     foreach ($data as $item) {
                         $t = new $target_class($this->persistence);
                         $t->loadBy($field, trim($item));
-                        $this->ref($this->get_class_name($bridge_class))->addCondition($target_their_field, $t[$target_class::our_field])->each(function ($m) {
+                        $this->ref(basename($bridge_class))->addCondition($target_their_field, $t[$target_class::our_field])->each(function ($m) {
                             $m->delete();
                         });
                     }
@@ -90,7 +90,7 @@ trait ManyToMany
                     return false;
                 }
             }
-            return $this->ref($this->get_class_name($bridge_class))->ref($this->get_class_name($target_class));
+            return $this->ref(basename($bridge_class))->ref(basename($target_class));
         });
 
         $this->_m2m['has'.$target] = [$target_class, $bridge_class, $default_field];
@@ -101,11 +101,11 @@ trait ManyToMany
 
             if (!empty($data)) {
                 $data = is_array($data) ? $data : explode(',', $data);
-                return $this->ref($this->get_class_name($bridge_class))->ref($this->get_class_name($target_class))->addCondition($field, $data)->action('count')->getOne();
+                return $this->ref(basename($bridge_class))->ref(basename($target_class))->addCondition($field, $data)->action('count')->getOne();
             }
             else
             {
-                return $this->ref($this->get_class_name($bridge_class))->ref($this->get_class_name($target_class))->action('count')->getOne();
+                return $this->ref(basename($bridge_class))->ref(basename($target_class))->action('count')->getOne();
             }
         });
     }
@@ -117,8 +117,8 @@ trait ManyToMany
 
         $this->addField($a_their_field);
         $this->addField($b_their_field);
-        $this->hasOne($this->get_class_name($a), [$a, 'our_field'=> $a_their_field, 'their_field'=>$a::our_field]);
-        $this->hasOne($this->get_class_name($b), [$b, 'our_field'=> $b_their_field, 'their_field'=>$b::our_field]);
+        $this->hasOne(basename($a), [$a, 'our_field'=> $a_their_field, 'their_field'=>$a::our_field]);
+        $this->hasOne(basename($b), [$b, 'our_field'=> $b_their_field, 'their_field'=>$b::our_field]);
     }
 
     public function get_zombie(&$target_class, &$bridge_class, &$default_field)
@@ -137,10 +137,5 @@ trait ManyToMany
     {
         $bridge_class = is_object($bridge_class) ? get_class($bridge_class) : $bridge_class;
         return !empty($target_class::their_field[$bridge_class]) ? $target_class::their_field[$bridge_class] : (is_string($target_class::their_field) ? $target_class::their_field : $target_class::their_field[0]);
-    }
-
-    public function get_class_name($classname)
-    {
-        return basename($classname);
     }
 }
